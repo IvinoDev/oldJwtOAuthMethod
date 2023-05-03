cbddcf2940388121bbfc93b6dd122a03b5b8e1b6
IvinoDev <kayantao@protonmail.com>
2023-05-03 09:46:26 +0000

    first commit

---
+++ src/main/java/com/example/demojwtoauth/DemoJwtOauthApplication.java
@@ -0,0 +1,13 @@
+package com.example.demojwtoauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoJwtOauthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoJwtOauthApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/demojwtoauth/DemoJwtOauthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demojwtoauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoJwtOauthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
